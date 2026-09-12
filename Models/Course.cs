9089a2eb7f6aa25ddebff529023777ce45341023
yasin sistani <80myszk80@gmail.com>
2026-09-12 15:50:47 +0330

    feat : initialize course

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Text;
 
 namespace CourseSelectionSystem.Models
 {
     public class Course
     {
+        // name and units create first in constructor 
+        // mifterm and final grade in steps in term extended
+        public string Name { get; }
+        public int Units {  get; }
+        public float MidtermGrade;
+        
+        public float FinalGrade;
 
+        public Course(string name , int unit)
+        {
+            Name = name;
+            Units = unit;
+        }
     }
 }
