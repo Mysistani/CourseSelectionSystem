f23e527578751ebc9e28ed59d07b27a086eda642
yasin sistani <80myszk80@gmail.com>
2026-09-12 16:04:49 +0330

    add enrollment and update student logic

---
+++ Models/Enrollment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseSelectionSystem.Models
+{
+    public class Enrollment
+        // in class namayande entekhabe yek dars tavasote daneshjo ast
+
+    {
+        public Course course { get;}
+
+
+
+        public double? MidtermGrade { get; private set; }
+        public double? FinalGrade { get; private set; }
+
+        public Enrollment(Course course)
+        {
+            Course = course;
+        }
+
+        public void RecordMidtermGrade(double grade)
+        {
+            ValidateGrade(grade);
+            MidtermGrade = grade;
+        }
+
+        public void RecordFinalGrade(double grade)
+        {
+            ValidateGrade(grade);
+            FinalGrade = grade;
+        }
+
+        private static void ValidateGrade(double grade)
+        {
+            if (grade < 0 || grade > 20)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(grade),
+                    "نمره باید بین صفر تا ۲۰ باشد.");
+            }
+        }
+    }
+}
+
--- Models/Student.cs
+++ Models/Student.cs
@@ -6,6 +6,16 @@ namespace CourseSelectionSystem.Models
 {
     class Student :  Person
     {
-
+        private readonly List<Enrollment> _enrollments =
+            new List<Enrollment>();
+    public IReadOnlyList<Enrollment> Enrollments { get { return _enrollments; }
+    public Student(string name , int age ): base(name, age)
+        { }
+    public void AddCourse (Course course)
+        {
+            Enrollment enrollment = new Enrollment(course);
+            _enrollments.Add(enrollment);
+        }
     }
+
 }
